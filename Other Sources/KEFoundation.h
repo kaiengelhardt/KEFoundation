45a48b81a7f98a3623b7ea1d0d912155b6f3d30e
Kai Engelhardt <kai@kaiengelhardt.com>
2021-08-08 19:25:36 +0200

    Initial Commit

---
+++ Other Sources/KEFoundation.h
@@ -0,0 +1,14 @@
+//
+//  KEFoundation.h
+//  KEFoundation
+//
+//  Created by Kai Engelhardt on 08.08.21.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for KEFoundation.
+FOUNDATION_EXPORT double KEFoundationVersionNumber;
+
+//! Project version string for KEFoundation.
+FOUNDATION_EXPORT const unsigned char KEFoundationVersionString[];
---
+++ Sources/Dummy.swift
@@ -0,0 +1,8 @@
+//
+//  Dummy.swift
+//  KEFoundation
+//
+//  Created by Kai Engelhardt on 08.08.21.
+//
+
+import Foundation
