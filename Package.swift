03cfcb9590e362e5c7416e6c39c06bdfbd197fff
Kai Engelhardt <kai@kaiengelhardt.com>
2026-06-09 12:52:07 +0200

    Update Consti dependency

--- Package.swift
+++ Package.swift
@@ -19,7 +19,7 @@ let package = Package(
 		),
 	],
 	dependencies: [
-		.package(url: "https://github.com/kaiengelhardt/consti", from: "1.0.1"),
+		.package(url: "https://github.com/kaiengelhardt/consti", from: "1.0.3"),
 	],
 	targets: [
 		.target(
